f9f9f8f50011ea93918814d8c56ba7a47f5aba75
Aryan Yadav <yadavaryan2003@gmail.com>
2023-12-03 23:38:33 +0530

    First Commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  Footer,
+  SuperQuality,
+  CustomerReviews,
+  SpecialOffer,
+  Subscribe,
+};
